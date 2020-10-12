2af49ce011f1f06c9c5293d0e586dcf2fad367ac
Oliver Okrongli <oliver.okrongli@infix.de>
2020-10-12 21:46:57 +0200

    Initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,49 @@
+plugins {
+    kotlin("multiplatform") version "1.4.10"
+    application
+}
+group = "me"
+version = "1.0-SNAPSHOT"
+
+repositories {
+    mavenCentral()
+    jcenter()
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/ktor")
+    }
+    maven {
+        url = uri("https://dl.bintray.com/kotlin/kotlinx")
+    }
+}
+kotlin {
+    jvm {
+        compilations.all {
+            kotlinOptions.jvmTarget = "11"
+        }
+        withJava()
+    }
+    sourceSets {
+        val commonMain by getting
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
+        }
+        val jvmMain by getting {
+            dependencies {
+                implementation("io.ktor:ktor-server-netty:1.4.0")
+                implementation("io.ktor:ktor-html-builder:1.4.0")
+                implementation("org.jetbrains.kotlinx:kotlinx-html-jvm:0.7.2")
+            }
+        }
+        val jvmTest by getting {
+            dependencies {
+                implementation(kotlin("test-junit"))
+            }
+        }
+    }
+}
+application {
+    mainClassName = "ServerKt"
+}
---
+++ src/jvmTest/kotlin/example.kt
@@ -0,0 +1,11 @@
+/**
+ *
+ */
+import kotlin.test.Test
+
+class ExampleTests {
+    @Test
+    fun test1() {
+        println("Running test1")
+    }
+}
